c2ffbf232993c3b0e4edfdbf0bfd9b64ab438d40
mvnueloc <volguinc1700@alumno.ipn.mx>
2024-07-13 02:12:54 -0600

    feat: initialize React project with base template
    - Set up a new React application using Create React App
    - Add initial project structure
    - Include basic dependencies and scripts

---
+++ README.md
@@ -0,0 +1,12 @@
+# DevInterview
+
+Quick Start
+
+Clone the repository, install the dependencies and run de local server:
+
+```
+git clone
+
+npm install
+npm run dev
+```
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="h-screen w-screen bg-gray-900 text-gray-100 flex justify-center items-center">
+        <h1 className="text-3xl font-semibold">DevInterview</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
