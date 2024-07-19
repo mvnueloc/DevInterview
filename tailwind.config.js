b47c1e9a8b4f2ad6057f8398df9d78e732547c2d
danielctecla <dcatongat2000@alumno.ipn.mx>
2024-07-19 14:07:03 -0600

    [DI-11-Design] Fix: Se soluciono el error del fondo
    - Se agrego la imagen de stats de ejemplo, para la siguiente fase
    - Se modifico index.html para arreglar el bug del fondo
    - Se comento el linear-gradient del archivo tailwind
    - Se borraron los stylos.css dentro de pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#1A1A1A",
+        primary: "#151719",
         secundary: "#3168CB",
       },
       backgroundImage: {
-        "custom-degrad":
-          "linear-gradient(90.21deg, #1A1A1A -5.91%, #232323 111.58%)",
+        // "custom-degrad":
+        //   "linear-gradient(90.21deg, #1A1A1A -5.91%, #232323 111.58%)",
       },
       fontFamily: {
         jetbrains: ['"JetBrains Mono"', "monospace"],
