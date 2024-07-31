2383524bec1d9067156c9529441456d3ef8678f9
mvnueloc <volguinc1700@alumno.ipn.mx>
2024-07-30 18:06:48 -0600

    [DI-12-Enhancement]
    - Se agrego el modulo framer-motion y react-typed
    - Se cambiaron las imagenes public/images/hero-image-crop.png y  public/images/hero-image.png
    - Se agregaron animaciones a los componentes:
      - src/components/main/BigTechs.jsx
      - src/components/main/Hero.jsx
      - src/components/main/Instructions.jsx
      - src/components/main/Metrics.jsx
      - src/components/main/MetricsItems.jsx
      - src/pages/Main.jsx
    - Se agrego la carpeta src/components/common/Animations/ que contiene las animaciones
    - Se edito el estilo de src/pages/Main.jsx en cuestion de dimenciones
    
    Resolves #5

--- src/components/main/Hero.jsx
+++ src/components/main/Hero.jsx
@@ -1,31 +1,68 @@
-import { Link } from 'react-router-dom';
-import Github from '../icons/Github';
+import { Link } from "react-router-dom";
+import Github from "../icons/Github";
+import { motion } from "framer-motion";
+import { ReactTyped } from "react-typed";
 
 const Hero = () => {
-    return (
-        <div className="pt-8 pb-24 font-onest grid-container">
-            <div className="base:w-full ">
-                <h1 className="text-3xl md:text-5xl font-bold mb-0.5">
-                    Mejora tus habilidades técnicas con ayuda de Inteligencia Artificial
-                </h1>
-                <p className="mt-6 text-base text-[#C8C8C8]">
-                    Entrena tus habilidades técnicas con IA: responde preguntas y recibe evaluaciones instantáneas.
-                </p>
-                <div className="inline-flex mt-7 gap-4">
-                    <Link to="/practicar" className='bg-[#4545E6] px-6 pt-[14px] pb-3 rounded text-sm font-medium neon-shadow'>Prácticar</Link>
-                    <a href="https://github.com/mvnueloc/DevInterview" className='inline-flex bg-black px-6 pt-[14px] items-center gap-2 pb-3 rounded font-medium text-sm '>
-                        <Github className='size-4'/>
-                        Repositorio
-                    </a>
-                </div>
-            </div>
-            <div className="w-full">
-                <img src="images/hero-image.png" alt="Dev Interview Presentación" className='base:hidden img-gradient-bottom rounded-b-lg' />
-                <img src="images/hero-image-crop.png" alt="Dev Interview Presentación" className='w-full hidden base:block img-gradient-right' />   
-            </div>
-        </div>
-    );
-};
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      className="h-full font-onest grid-container items-center">
+      <div className="base:w-full ">
+        <h1 className="text-3xl md:text-5xl font-bold mb-0.5">
+          Mejora tus habilidades técnicas con ayuda de Inteligencia Artificial
+        </h1>
 
+        <h2 className="text-[#C8C8C8] text-xl md:text-2xl font-semibold mt-3">
+          Creado para{" "}
+          <span className="text-transparent bg-clip-text bg-gradient-to-r to-[#0E54BC] from-[#6b94d2]">
+            <ReactTyped
+              strings={[
+                "Frontend",
+                "Backend",
+                "FullStack",
+                "Software Developer",
+              ]}
+              typeSpeed={50}
+              backSpeed={100}
+              loop></ReactTyped>
+          </span>
+        </h2>
+
+        <p className="mt-6 text-base text-[#C8C8C8]">
+          Responde preguntas y recibe evaluaciones instantáneas.
+        </p>
+
+        <div className="inline-flex mt-7 gap-4">
+          <Link
+            to="/practicar"
+            className="bg-[#4545E6] hover:shadow-lg hover:shadow-[#4545E6]/50 hover:scale-105 transition-all duration-500 px-6 pt-[14px] pb-3 rounded text-sm font-medium neon-shadow">
+            Prácticar
+          </Link>
+          <a
+            href="https://github.com/mvnueloc/DevInterview"
+            className="inline-flex bg-black hover:shadow-lg hover:shadow-gray-700/50 hover:scale-105 transition-all duration-500 px-6 pt-[14px] items-center gap-2 pb-3 rounded font-medium text-sm ">
+            <Github className="size-4" />
+            Repositorio
+          </a>
+        </div>
+      </div>
+      <div className="w-full">
+        <img
+          src="images/hero-image.png"
+          alt="Dev Interview Presentación"
+          className="base:hidden img-gradient-bottom rounded-b-lg"
+        />
+        <img
+          src="images/hero-image-crop.png"
+          alt="Dev Interview Presentación"
+          className="w-full hidden base:block img-gradient-right"
+        />
+      </div>
+    </motion.div>
+  );
+};
 
 export default Hero;
--- src/components/main/Instructions.jsx
+++ src/components/main/Instructions.jsx
@@ -2,84 +2,93 @@ import Question from "../icons/Question";
 import TechnicalIcon from "../icons/Technical";
 import CodeIcon from "../icons/Code";
 import BulletPointIcon from "../icons/BulletPoint";
+import FadeInLeft from "../common/Animations/FadeInLeft";
+import FadeInRight from "../common/Animations/FadeInRight";
 
 const Instructions = () => {
-    return (
-        <div className="w-full pt-16 pb-20">
-            <h2 className="text-center text-2xl md:text-4xl font-semibold">
-                
-                Estás a un Paso de tu Empleo Soñado
-            </h2>
-            <p className="m-auto max-w-3xl text-lg text-center my-10">
-                Tendrás una entrevista técnica evaluada por una Inteligencia Artificial. ¿Estás listo para demostrar tus habilidades?, esto es lo que necesitas saber:
-            </p>
-            <div className="w-full flex flex-col gap-6 base:flex-row">
-                <article className="w-full min-h-full p-6 bg-[#2f2f31]/30 flex flex-col gap-y-4  md:flex-row base:flex-col items-center rounded-md">
-                    <div className="flex-1">
-                        <h3 className="inline-flex text-base bg-[#97E1C5]/50 rounded-full px-3 py-1 font-semibold items-center">
-                            <TechnicalIcon className="size-5" color="#97E1C5"/>
-                            <span className="ml-2 text-[#97E1C5]">
-                                Preguntas Técnicas
-                            </span>
-                        </h3>
-                        <div className="w-full inline-flex text-base mt-4">
-                            <BulletPointIcon className="size-5" color="#97E1C5"/>
-                            Duración de 2 a 5 minutos por pregunta. 
-                        </div>
-                        <div className="inline-flex text-base">
-                            <BulletPointIcon className="size-5" color="#97E1C5"/>
-                            Evaluación de tu conocimiento teórico. 
-                            
-                        </div>
-                    </div>
-                    <div className="flex-1">
-                        <h3 className="inline-flex text-base bg-[#E4607B]/50 rounded-full px-3 py-1 font-semibold items-center">
-                            <CodeIcon className="size-5" color="#E4607B"/>
-                            <span className="ml-2 text-[#E4607B]">
-                                Prueba de Código
-                            </span>
-                        </h3>
-                        <div className="w-full inline-flex mt-4 text-base">
-                            <BulletPointIcon className="size-5" color="#E4607B"/>
-                                Duración de 15 a 20 minutos. 
-                        </div>
-                        <div className="inline-flex text-base">
-                            <BulletPointIcon className="size-5" color="#E4607B"/>
-                            Resolución de problemas prácticos.
-                        </div>
-                    </div>
-                </article>
-                <div className="base:min-w-[60%] base:max-w-[60%]">
-                    <article className="w-full h-full flex flex-col justify-center p-6 bg-[#2f2f31]/30 rounded-md gap-4">
-                        <div>
-                            <h3 className="inline-flex text-base bg-[#DFDE51]/50 rounded-full px-3 py-1 font-semibold items-center">
-                                <Question className="size-5" color="#DFDE51"/>
-                                <span className="ml-2 text-[#DFDE51]">
-                                    Información
-                                </span>
-                            </h3>
-                        </div>
-                        <div className="w-full">
-                                <h4 className="text-base font-semibold">¿Se necesita una API Key?</h4>
-                                <p className="mt-3 text-base">
-                                    La aplicación ofrece un número limitado de demos diarias. Al agotarse, los usuarios deben clonar la aplicación y seguir las instrucciones del {" "}
-                                    <span className="bg-gray-100/[0.3] px-1 rounded-md">README</span>{" "}
-                                    para continuar usándola.
-                                </p>
-                        </div>
-                        <div className=" w-full">
-                            <h4 className="text-base font-semibold">¿Qué modelo usa?</h4>
-                            <p className="mt-3 text-base">
-                                Implementado con {" "}
-                                <span className="bg-gray-100/[0.3] px-1 rounded-md">Gemini-1.5-pro</span>{" "}
-                                por defecto. Cambia fácilmente a otros modelos con pocas líneas de código.
-                            </p>
-                        </div>
-                    </article>
-                </div>
+  return (
+    <div className="w-full pb-20">
+      <h2 className="text-center text-2xl md:text-4xl font-semibold">
+        Estás a un Paso de tu Empleo Soñado
+      </h2>
+      <p className="m-auto max-w-3xl text-lg text-center my-10">
+        Tendrás una entrevista técnica evaluada por una Inteligencia Artificial.
+        ¿Estás listo para demostrar tus habilidades?, esto es lo que necesitas
+        saber:
+      </p>
+      <div className="w-full flex flex-col gap-6 base:flex-row overflow-x-hidden">
+        <FadeInLeft>
+          <article className="w-full min-h-full p-6 bg-[#2f2f31]/30 flex flex-col gap-y-4  md:flex-row base:flex-col items-center rounded-md">
+            <div className="flex-1">
+              <h3 className="inline-flex text-base bg-[#97E1C5]/50 rounded-full px-3 py-1 font-semibold items-center">
+                <TechnicalIcon className="size-5" color="#97E1C5" />
+                <span className="ml-2 text-[#97E1C5]">Preguntas Técnicas</span>
+              </h3>
+              <div className="w-full inline-flex text-base mt-4">
+                <BulletPointIcon className="size-5" color="#97E1C5" />
+                Duración de 2 a 5 minutos por pregunta.
+              </div>
+              <div className="inline-flex text-base">
+                <BulletPointIcon className="size-5" color="#97E1C5" />
+                Evaluación de tu conocimiento teórico.
+              </div>
             </div>
+            <div className="flex-1">
+              <h3 className="inline-flex text-base bg-[#E4607B]/50 rounded-full px-3 py-1 font-semibold items-center">
+                <CodeIcon className="size-5" color="#E4607B" />
+                <span className="ml-2 text-[#E4607B]">Prueba de Código</span>
+              </h3>
+              <div className="w-full inline-flex mt-4 text-base">
+                <BulletPointIcon className="size-5" color="#E4607B" />
+                Duración de 15 a 20 minutos.
+              </div>
+              <div className="inline-flex text-base">
+                <BulletPointIcon className="size-5" color="#E4607B" />
+                Resolución de problemas prácticos.
+              </div>
+            </div>
+          </article>
+        </FadeInLeft>
+        <div className="base:min-w-[60%] base:max-w-[60%] overflow-x-hidden">
+          <FadeInRight>
+            <article className="w-full h-full flex flex-col justify-center p-6 bg-[#2f2f31]/30 rounded-md gap-4">
+              <div>
+                <h3 className="inline-flex text-base bg-[#DFDE51]/50 rounded-full px-3 py-1 font-semibold items-center">
+                  <Question className="size-5" color="#DFDE51" />
+                  <span className="ml-2 text-[#DFDE51]">Información</span>
+                </h3>
+              </div>
+              <div className="w-full">
+                <h4 className="text-base font-semibold">
+                  ¿Se necesita una API Key?
+                </h4>
+                <p className="mt-3 text-base">
+                  La aplicación ofrece un número limitado de demos diarias. Al
+                  agotarse, los usuarios deben clonar la aplicación y seguir las
+                  instrucciones del{" "}
+                  <span className="bg-gray-100/[0.3] px-1 rounded-md">
+                    README
+                  </span>{" "}
+                  para continuar usándola.
+                </p>
+              </div>
+              <div className=" w-full">
+                <h4 className="text-base font-semibold">¿Qué modelo usa?</h4>
+                <p className="mt-3 text-base">
+                  Implementado con{" "}
+                  <span className="bg-gray-100/[0.3] px-1 rounded-md">
+                    Gemini-1.5-pro
+                  </span>{" "}
+                  por defecto. Cambia fácilmente a otros modelos con pocas
+                  líneas de código.
+                </p>
+              </div>
+            </article>
+          </FadeInRight>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Instructions;
